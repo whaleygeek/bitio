3f5261d80dc7f3596f4089b6f822f23669ab5a27
David Whale <david@thinkingbinaries.com>
2017-07-11 23:48:36 +0100

    Corrected axes problem on mc_tilt. The accelerometer uses x/y but minecraft uses x/z

--- src/tilt_mc.py
+++ src/tilt_mc.py
@@ -6,11 +6,11 @@ mc = minecraft.Minecraft.create()
 
 while True:
     pos = mc.player.getTilePos()
-    x = microbit.accelerometer.get_x()/300
-    y = microbit.accelerometer.get_y()/300
+    x = microbit.accelerometer.get_x()/300 # -ve=left/+ve=right
+    y = microbit.accelerometer.get_y()/300 # -ve=forward/+ve=backward
 
-    pos.x += x
-    pos.y += y
+    pos.x += x # east/west
+    pos.z += y # north/south
 
     mc.player.setTilePos(pos.x, pos.y, pos.z)
 
