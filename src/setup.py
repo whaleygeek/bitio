d72a47ca9dd9d9d1b6dabd9d9cf97723041b9f27
musabkilic <musabkilic@protonmail.com>
2019-09-01 16:19:48 +0300

    Add setup.py to allow pip install

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bitio',
+      version='0.48c',
+      description='micro:bit I/O library for Python',
+      url='https://github.com/whaleygeek/bitio',
+      author='David Whale',
+      license='MIT',
+      packages=['microbit'])
